62935afb2c44fc04bc628560b0437df6f118acf2
Kanescode <rlrlok@126.com>
2015-11-08 17:19:06 +0800

    modify config

--- config.py
+++ config.py
@@ -56,6 +56,17 @@ class ProductionConfig(Config):
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
+class HerokuConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
+
 config = {
     'development': DevelopmentConfig,
     'test': TestingConfig,
